807a7edfac012fa401236fc9f716ce22217a3bb7
alex.matos <alex.matos@ceuma.br>
2022-11-25 09:37:43 -0300

    feat: adicionando title à página de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import { FormSection } from '@/components/pages/login/FormSection'
 
 const Login = () => {
-  return <FormSection />
+  return (
+    <>
+      <Head>
+        <title>Farmy | Login</title>
+      </Head>
+      <FormSection />
+    </>
+  )
 }
 
 export default Login
